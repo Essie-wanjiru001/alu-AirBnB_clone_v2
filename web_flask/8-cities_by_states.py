11b5b6e27f5cf0359fbe0183c35d4e16751a9b9e
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2023-06-30 13:47:33 +0000

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" Starts a Flask application related to HBNB. """
+"""
+    python script that starts a Flask web application
+"""
 
-from os import getenv
-from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states')
+def states_list():
+    """
+        Return: HTML page with list of states
+    """
+    path = '8-cities_by_states.html'
+    states = storage.all('State')
+    return render_template(path, states=states)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database session after each request."""
+def app_teardown(arg=None):
+    """
+        Clean-up session
+    """
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """
-        Flask route at /cities_by_states.
-        Displays the list of the Cities in the database with their states.
-    """
-    states = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states)
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
